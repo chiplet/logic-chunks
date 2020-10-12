021935cee77d1cc45b11799577a60642d67ffef2
Verneri Hirvonen <verneri.hirvonen.2@gmail.com>
2020-10-12 19:34:00 +0300

    Remove useless module instantiation. Add build dirs to gitignore.

--- src/hello_world/icarus/hello_icarus.v
+++ src/hello_world/icarus/hello_icarus.v
@@ -1,7 +1,6 @@
 // A minimal hello world verilog design simulated with Icarus Verilog.
 
 module hello_icarus;
-    mod2 m();
     initial begin
         $display("Hello, world!");
         $finish;
